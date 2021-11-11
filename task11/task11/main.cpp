19714b72910f680c29d1edf5bd895d6a6bdd1ddc
selekzioner <a.a.martushev@gmail.com>
2021-11-11 15:17:43 +0700

    task11

--- task11/task11/main.cpp
+++ task11/task11/main.cpp
@@ -1,76 +1,204 @@
-#define N 1000
+#pragma once
+#include <functional>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include <sciplot/sciplot.hpp>
+#include "../GNUDraw/GNUDraw/GNUDrawLib.h"
 
-using namespace std;
+class ShcrodingerSolver
+{
+public:
+  std::vector<double> x, psi_an;
+  std::vector<std::vector<double>> psi;
 
-double l = 0, r = 7, h = (r - l) / N;
-double energy = 0, product, normalization, previous, epsilon = 0.0001;
+  std::tuple<std::vector<double>, std::vector<double>, std::vector<double>> A(std::vector<double> x, std::function<double(double)> U, int N)
+  {
+    double h = x[1] - x[0];
+
+    auto a = std::vector<double>(N, -1.0 / (2 * h * h));
+
+    a[0] = 0;
+
+    auto b = std::vector<double>(x.size());
+
+    for (int i = 0; i < x.size(); ++i)
+    {
+      b[i] = 1.0 / (h * h) + U(x[i]);
+    }
+
+
+    auto c = std::vector<double>(N, -1.0 / (2 * h * h));
+
+    for (auto element : c)
+    {
+      element = -1.0 / (2 * h * h);
+    }
+
+    c[N - 1] = 0;
+
+    return std::tuple<std::vector<double>, std::vector<double>, std::vector<double>>(a, b, c);
+  }
+
+  std::vector<double> copy(std::vector<double> a)
+  {
+    auto b = std::vector<double>(a.size());
+
+    for (int i = 0; i < a.size(); ++i)
+    {
+      b[i] = a[i];
+    }
+
+    return b;
+  }
+
+  std::vector<double> TridiagMatrixAlg(std::vector<double> a, std::vector<double> b, std::vector<double> c, std::vector<double> d, int N)
+  {
+    std::vector<double> y = std::vector<double>(N, 0);
+
+    for (int i = 1; i < N; i++)
+    {
+      auto xi = a[i] / b[i - 1];
+      a[i] = 0;
+      b[i] -= xi * c[i - 1];
+      d[i] -= xi * d[i - 1];
+    }
+
+    y[N - 1] = d[N - 1] / b[N - 1];
+    for (int i = N - 2; i >= 0; i--)
+    {
+      y[i] = 1 / b[i] * (d[i] - c[i] * y[i + 1]);
+    }
+    return y;
+  }
+
+  double norm(std::vector<double> a)
+  {
+    double res = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+      res += a[i] * a[i];
+    }
+
+    return sqrt(res);
+  }
+
+  double inner(std::vector<double> a, std::vector<double> b)
+  {
+    double res = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+      res += a[i] * b[i];
+    }
+
+    return res;
+  }
+
+  void multiply(std::vector<double>& a, double b)
+  {
+    for (int i = 0; i < a.size(); i++)
+    {
+      a[i] *= b;
+    }
+  }
+
+  void Orthogonalization(std::vector<double>& psi_next, std::vector<std::vector<double>>& psi_prevs)
+  {
+    for (auto psi : psi_prevs)
+    {
+      auto normal = norm(psi);
+      auto innerProduct = inner(psi_next, psi);
+      auto multiplier = innerProduct / normal;
+      for (int i = 0; i < psi_next.size(); i++)
+      {
+        psi_next[i] -= psi[i] * multiplier;
+      }
+    }
+  }
+
+  std::vector<double> E;
+
+  void solve(double left, double right, int N, int iterations, int levels, std::function<double(double)> U)
+  {
+    reallocate(N);
+
+    E = std::vector<double>();
+
+    psi = std::vector<std::vector<double>>();
+    double dx = (right - left) / (N - 1);
+    x = std::vector<double>();
+    for (int i = 0; i < N; i++)
+    {
+      x.push_back(left + dx * i);
+    }
+
+    auto abc = A(x, U, N);
+    auto a = std::get<0>(abc);
+    auto b = std::get<1>(abc);
+    auto c = std::get<2>(abc);
+
+    std::vector<double> psi_0 = std::vector<double>();
+    for (int i = 0; i < N; i++)
+    {
+      psi_0.push_back(1.0 + ((double)i / (N - 1)));
+    }
+
+    for (int i = 0; i < levels; i++)
+    {
+      auto psiNext = std::vector<double>(psi_0);
+      Orthogonalization(psiNext, psi);
+      std::vector<double> psiPrev;
+
+      for (int k = 0; k < iterations; k++)
+      {
+        psiPrev = psiNext;
+        psiNext = TridiagMatrixAlg(copy(a), copy(b), copy(c), copy(psiNext), N);
+        Orthogonalization(psiNext, psi);
+      }
+
+      auto E0 = norm(psiPrev) / norm(psiNext);
+      E.push_back(E0);
+      multiply(psiNext, 1.0 / norm(psiNext));
+      psi.push_back(psiNext);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+      psi_an[i] = exp(-x[i] * x[i] / 2);
+    }
+
+    multiply(psi_an, 1.0 / norm(psi_an));
+  }
+
+  void reallocate(int N)
+  {
+    psi_an = std::vector<double>(N, 0);
+  }
+};
 
 int main()
 {
-  double x[N + 1], y[N + 1], a[N + 1], b[N + 1], c[N + 1], u[N + 1];
+  std::function<double(double)> U = [](double x) {return x * x / 2; };
 
-  for (int i = 0; i <= N; i++) {
-    x[i] = 1;
-    u[i] = (l + i * h) * (l + i * h) / 2;
-    a[i] = -1 / (2 * h  * h);
-    b[i] = -1 / (2 * h * h);
-    c[i] = 1 / (h * h) + u[i] - energy;
+  int N = 2000;
+  int iter = 21;
+  int levels = 4;
+
+  auto solver = ShcrodingerSolver();
+  solver.solve(-10, 10, N, iter, levels, U);
+
+  GNUDrawer gnuDrawer;
+  auto arr = Arrays::Construct();
+  arr->Add("x", solver.x);
+
+  for (int i = 0; i < solver.psi.size(); ++i)
+  {
+    arr->Add("psi " + std::to_string(i), solver.psi[i]);
   }
 
-  x[0] = 0; x[N] = 0;
-  a[0] = 0; a[N] = 0;
-  b[0] = 0; b[N] = 0;
-  c[0] = 1; c[N] = 1;
-
-  double alpha[N + 1], beta[N + 1];
-  alpha[1] = 1; beta[1] = 0;
-
-  previous = energy + 2 * epsilon;
-  while (abs(previous - energy) > epsilon) {
-    for (int i = 1; i < N; i++) {
-      alpha[i + 1] = -b[i] / (a[i] * alpha[i] + c[i]);
-      beta[i + 1] = (x[i] - a[i] * beta[i]) / (a[i] * alpha[i] + c[i]);
-    }
-
-    y[N] = (x[N] - a[N] * beta[N]) / (c[N] + a[N] * alpha[N]);
-    normalization = y[N] * y[N];
-
-    for (int i = N - 1; i >= 0; i--) {
-      y[i] = alpha[i + 1] * y[i + 1] + beta[i + 1];
-      normalization += y[i] * y[i];
-    }
-
-    product = 0;
-    for (int i = 0; i <= N; i++) {
-      product += x[i] * y[i];
-    }
-    previous = energy;
-    energy += 1. / product;
-
-    for (int i = 0; i <= N; i++) {
-      x[i] = y[i] / sqrt(normalization);
-    }
-
-    cout << energy << endl;
-
-    for (int i = 0; i <= N; i++) {
-      c[i] = 1. / (h * h) + u[i] - energy;
-    }
-  }
-
-  ofstream data;
-  data.open("data.txt");
-
-  for (int i = 0; i <= N; i++) {
-    data << l + i * h << "	" << abs(x[i]) << endl;
-  }
-
-  data.close();
+  arr->Add("psi_an", solver.psi_an);
+  gnuDrawer.Add(arr);
+  gnuDrawer.Draw();
 
   return 0;
 }
